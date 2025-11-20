6994d3e43083d51d789c9c847818e7b127a6cb2a
qeftser <qeftser1@gmail.com>
2025-11-19 18:32:37 -0700

    graph outline

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
-window.onload = loaded;
 
-/**
- * Simple Function that will be run when the browser is finished loading.
- */
-function loaded() {
-    // Assign to a variable so we can set a breakpoint in the debugger!
-    const hello = sayHello();
-    console.log(hello);
+function startChange(val) {
+   document.getElementById('start_pos_out').innerHTML = val;
 }
 
-/**
- * This function returns the string 'hello'
- * @return {string} the string hello
- */
-export function sayHello() {
-    return 'hello';
+function endChange(val) {
+   document.getElementById('end_pos_out').innerHTML = val;
 }
